a9dc756430f8fa6b8ec128a479aff933b4d66ff0
arshad <ak849496@gmail.com>
2024-01-09 19:03:28 +0500

    Initial Commit

---
+++ KidsStories/KidsStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsStoriesApp.swift
+//  KidsStories
+//
+//  Created by Arshad Mustafa on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct KidsStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
